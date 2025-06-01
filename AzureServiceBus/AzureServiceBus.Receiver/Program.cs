f6257aa9670b1e42c94d09b53be9759d353301ae
Mahmoud-Mi <mahmoud.e@miviewis.com>
2025-05-31 22:51:41 -0500

    Queues

--- AzureServiceBus/AzureServiceBus.Receiver/Program.cs
+++ AzureServiceBus/AzureServiceBus.Receiver/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Messaging.ServiceBus;
+using System.Collections.Generic;
+
+string connectionString = Environment.GetEnvironmentVariable("AzureServiceBusConnection");
+const string queueName = "gendiQU";
+
+// Create client
+await using var client = new ServiceBusClient(connectionString);
+
+// ===== SEND MESSAGE =====
+ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+IReadOnlyList<ServiceBusReceivedMessage> messages = await receiver.ReceiveMessagesAsync(maxMessages: 3);
+foreach (ServiceBusReceivedMessage message in messages)
+{
+    string body = message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // Complete the message so it's removed from the queue
+    await receiver.CompleteMessageAsync(message);
+}
--- AzureServiceBus/AzureServiceBus.Sender/Program.cs
+++ AzureServiceBus/AzureServiceBus.Sender/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Messaging.ServiceBus;
+
+
+string connectionString = Environment.GetEnvironmentVariable("AzureServiceBusConnection");
+
+const string queueName = "gendiQU";
+
+// Create client
+await using var client = new ServiceBusClient(connectionString);
+
+// ===== SEND MESSAGE =====
+ServiceBusSender sender = client.CreateSender(queueName);
+for (int i = 0; i < 3; i++)
+{
+    var message = new ServiceBusMessage($"Hello World {i + 1}!");
+    await sender.SendMessageAsync(message);
+    Console.WriteLine($"Message {i + 1} sent.");
+}
