8fa9863516f89426fd3ef9d0ad72996100ee42fb
Mahmoud-Mi <mahmoud.e@miviewis.com>
2025-06-01 00:12:46 -0500

    --

--- AzureServiceBus/AzureServiceBus.Receiver/Program.cs
+++ AzureServiceBus/AzureServiceBus.Receiver/Program.cs
@@ -1,19 +1,34 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Amqp.Framing;
 
 string connectionString = Environment.GetEnvironmentVariable("AzureServiceBusConnection");
 const string queueName = "gendiQU";
+const string topicName = "genditc";
+const string subscriptionName = "gendisubscription";
 
 // Create client
 await using var client = new ServiceBusClient(connectionString);
 
-// ===== Receive MESSAGE =====
-ServiceBusReceiver receiver = client.CreateReceiver(queueName);
-IReadOnlyList<ServiceBusReceivedMessage> messages = await receiver.ReceiveMessagesAsync(maxMessages: 3);
-foreach (ServiceBusReceivedMessage message in messages)
+// ===== Receive MESSAGE From Queue =====
+ServiceBusReceiver queueReceiver = client.CreateReceiver(queueName);
+IReadOnlyList<ServiceBusReceivedMessage> queueMessages = await queueReceiver.ReceiveMessagesAsync(maxMessages: 3);
+foreach (ServiceBusReceivedMessage message in queueMessages)
 {
     string body = message.Body.ToString();
     Console.WriteLine($"Received: {body}");
 
     // Complete the message so it's removed from the queue
-    await receiver.CompleteMessageAsync(message);
+    await queueReceiver.CompleteMessageAsync(message);
+}
+
+// ===== Receive MESSAGE From Topic =====
+ServiceBusReceiver receiver = client.CreateReceiver(topicName, subscriptionName);
+var received = await receiver.ReceiveMessagesAsync(maxMessages: 5, maxWaitTime: TimeSpan.FromSeconds(10));
+
+foreach (var msg in received)
+{
+    Console.WriteLine($"Received: {msg.Body}");
+
+    // Complete = remove from subscription
+    await receiver.CompleteMessageAsync(msg);
 }
--- AzureServiceBus/AzureServiceBus.Sender/Program.cs
+++ AzureServiceBus/AzureServiceBus.Sender/Program.cs
@@ -1,16 +1,23 @@
 ﻿using Azure.Messaging.ServiceBus;
 
-string connectionString = Environment.GetEnvironmentVariable("AzureServiceBusConnection");
+string connectionString = Environment.GetEnvironmentVariable("AzureServiceBusConnectionString");
 const string queueName = "gendiQU";
+const string topicName = "genditc";
 
-// Create client
+// ===== Create client =====
 await using var client = new ServiceBusClient(connectionString);
 
-// ===== SEND MESSAGE =====
-ServiceBusSender sender = client.CreateSender(queueName);
+// ===== SEND MESSAGE To Queue =====
+ServiceBusSender queueSender = client.CreateSender(queueName);
 for (int i = 0; i < 3; i++)
 {
-    var message = new ServiceBusMessage($"Hello World {i + 1}!");
-    await sender.SendMessageAsync(message);
+    var QueueMessage = new ServiceBusMessage($"Hello World {i + 1}!");
+    await queueSender.SendMessageAsync(QueueMessage);
     Console.WriteLine($"Message {i + 1} sent.");
 }
+
+// ===== SEND MESSAGE TO TOPIC =====
+ServiceBusSender topicSender = client.CreateSender(topicName);
+ServiceBusMessage topicMessage = new("Hello from Topic2");
+await topicSender.SendMessageAsync(topicMessage);
+Console.WriteLine("Message sent to topic.");
