3f3cc21a35032f00a57092cc91216c363d65ac5a
Mahmoud-Mi <mahmoud.e@miviewis.com>
2025-06-07 08:07:16 -0500

    http calls

---
+++ HttpConsumer/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ HttpClient client = new HttpClient();
+string url = "http://localhost:7001/WeatherForecast";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode(); // Throws if not 2xx
+
+string responseBody = await response.Content.ReadAsStringAsync();
+Console.WriteLine(responseBody);
+Console.ReadLine();
