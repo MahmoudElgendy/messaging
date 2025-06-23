ef7283ed47edf4c70375f45e441f3139690ced2e
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2025-06-23 10:46:07 -0500

    set client and cousume the back end

---
+++ grpc/GRPCClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GRPCServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5026");
+var client = new Greeter.GreeterClient(channel);
+
+Console.Write("Enter your name: ");
+var name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Server says: {reply.Message}");
+Console.ReadLine();
