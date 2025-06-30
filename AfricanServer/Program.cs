b229de41c2d12e1d40916639ecd3cca9ae5660f6
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2025-06-30 13:26:49 -0500

    generate jwt

--- AfricanServer/Program.cs
+++ AfricanServer/Program.cs
@@ -1,16 +1,38 @@
 using africanserver.Services;
 using AfricanServer.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://localhost:7159",
+            ValidAudience = "https://localhost:7159",
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("mySuperSecureKey!mySuperSecureKey!!"))
+        };
 
+    });
+builder.Services.AddAuthorization();
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+builder.Logging.AddConsole();
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<ProductService>();
+app.MapGrpcService<AuthenticationService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ AfricanServer/Services/AuthenticationService.cs
@@ -0,0 +1,48 @@
+﻿using AfricanServer;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace africanserver.Services
+{
+    public class AuthenticationService : AuthProtoService.AuthProtoServiceBase
+    {
+        [AllowAnonymous]
+        public override async Task<CreateIdentityResponse> GenerateToken(Empty request, ServerCallContext context)
+        {
+            try
+
+            {
+                var expiration = DateTime.UtcNow.AddHours(1);
+                Claim[] claims = [new Claim(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString())];
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mySuperSecureKey!mySuperSecureKey!!"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    issuer: "https://localhost:7159",
+                    audience: "https://localhost:7159",
+                    claims: claims,
+                    expires: expiration,
+                    signingCredentials: creds
+                );
+                string _token = new JwtSecurityTokenHandler().WriteToken(token);
+                await Task.Delay(100); // Simulate some async work
+                return new CreateIdentityResponse
+                {
+                    Token = _token
+                };
+            }
+            catch (System.Exception ex)
+
+            {
+
+                Console.WriteLine("EXCEPTION: " + ex);
+                throw;
+            }
+        }
+    }
+}
